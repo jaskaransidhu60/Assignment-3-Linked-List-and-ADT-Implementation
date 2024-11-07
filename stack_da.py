1a6069249f0840ed32f0bf53cef5d78ae5ed123d
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-11-06 19:43:04 -0800

    Create stack_da.py

---
+++ stack_da.py
@@ -0,0 +1,22 @@
+# Name: JASKARAN SINGH SIDHU
+# OSU Email: sidhuja@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 3
+# Due Date: 4 November
+# Description: Implements a Stack ADT using a Dynamic Array from Assignment 2.
+# Provides the classic stack methods, such as push, pop, and top, with efficient O(1) operations for stack manipulation.
+
+from dynamic_array import DynamicArray
+
+class Stack:
+    def __init__(self):
+        self._da = DynamicArray()
+
+    def push(self, value: object) -> None:
+        pass  # implement
+
+    def pop(self) -> object:
+        pass  # implement
+    
+    def top(self) -> object:
+        pass  # implement
