fa474390e29ab7d6e19e8d11ab09b89675bf3baf
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-11-06 19:43:38 -0800

    Create queue_sa.py

---
+++ queue_sa.py
@@ -0,0 +1,22 @@
+# Name: JASKARAN SINGH SIDHU
+# OSU Email: sidhuja@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 3
+# Due Date: 4 November
+# Description: Implements a Queue ADT using a Static Array from Assignment 1 with a circular buffer.
+# Allows enqueue and dequeue operations with amortized O(1) efficiency and includes a front method to peek at the next item.
+
+from static_array import StaticArray
+
+class Queue:
+    def __init__(self):
+        pass  # initialize static array
+    
+    def enqueue(self, value: object) -> None:
+        pass  # implement enqueue
+
+    def dequeue(self) -> object:
+        pass  # implement dequeue
+    
+    def front(self) -> object:
+        pass  # implement front peek
