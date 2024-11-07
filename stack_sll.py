7d02d3575c519e7390d83810b3bb3948bac2b92f
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-11-06 20:03:49 -0800

    Update stack_sll.py

--- stack_sll.py
+++ stack_sll.py
@@ -17,20 +17,26 @@ class StackNode:
 class Stack:
     def __init__(self):
         self.top_node = None
+        self.size = 0  # Added size attribute to track stack size
 
     def push(self, value):
         new_node = StackNode(value)
         new_node.next = self.top_node
         self.top_node = new_node
+        self.size += 1
 
     def pop(self):
-        if not self.top_node:
+        if self.is_empty():
             raise StackException("Pop from empty stack")
         value = self.top_node.value
         self.top_node = self.top_node.next
+        self.size -= 1
         return value
 
     def top(self):
-        if not self.top_node:
+        if self.is_empty():
             raise StackException("Top of empty stack")
         return self.top_node.value
+
+    def is_empty(self):
+        return self.size == 0
