08361377a0a23ce7589874a423f87d8bad35db6c
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-11-06 19:44:44 -0800

    Update queue_sll.py

--- queue_sll.py
+++ queue_sll.py
@@ -1,1 +1,20 @@
+# Name: JASKARAN SINGH SIDHU
+# OSU Email: sidhuja@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 3
+# Due Date: 4 November
+# Description: Implements a Queue ADT using a Singly Linked List with support for efficient enqueue, dequeue, and front-peek operations.
+# This queue structure facilitates O(1) time complexity for each primary operation by maintaining both head and tail pointers.
 
+class Queue:
+    def __init__(self):
+        pass  # initialize linked list-based queue
+    
+    def enqueue(self, value: object) -> None:
+        pass  # implement enqueue
+    
+    def dequeue(self) -> object:
+        pass  # implement dequeue
+    
+    def front(self) -> object:
+        pass  # implement front peek
