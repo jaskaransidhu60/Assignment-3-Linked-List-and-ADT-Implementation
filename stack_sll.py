033834ad864c1a0922160d1af9cd66845d31fc18
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-11-06 19:43:57 -0800

    Create stack_sll.py

---
+++ stack_sll.py
@@ -0,0 +1,20 @@
+# Name: JASKARAN SINGH SIDHU
+# OSU Email: sidhuja@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 3
+# Due Date: 4 November
+# Description: Implements a Stack ADT using a Singly Linked List.
+# Utilizes a linked-node structure with constant time complexity for push, pop, and top operations, adhering to stack principles.
+
+class Stack:
+    def __init__(self):
+        pass  # Linked List based stack initialization
+
+    def push(self, value: object) -> None:
+        pass  # implement push
+    
+    def pop(self) -> object:
+        pass  # implement pop
+    
+    def top(self) -> object:
+        pass  # implement top peek
